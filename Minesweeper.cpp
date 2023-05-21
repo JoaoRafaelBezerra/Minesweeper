0f3fec21bed829d09e7fb0cbbebe38123c0f5458
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-05-21 15:27:02 -0300

    kkkkkkk

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -8,6 +8,8 @@ class CampoMinado
 {
     public:
         bool TemBomba(int linha,int coluna);
+        bool TemBombaPerto(int linha,int coluna);
+        int  ContaBomba(int linha, int coluna);
         void MostrarTabuleiro();
         void Inicializar();
         void AtualizarTabuleiro(int linha, int coluna);
@@ -40,7 +42,7 @@ void CampoMinado::Inicializar()
 void CampoMinado::MostrarTabuleiro()
 {
     for(int i = 0; i < 9; i++){
-        cout << i << " ";
+        cout << i + 1 << " ";
         for(int j = 0; j < 9; j++){
             if(tabuleiro[i][j] == 'B'){
                 cout << "[-] ";
@@ -51,7 +53,7 @@ void CampoMinado::MostrarTabuleiro()
         cout << endl;
     }
     for(int i = 0; i < 9; i++){
-        cout << "   " << i;
+        cout << "   " << i + 1;
     }
     cout << endl;
 } 
@@ -62,29 +64,93 @@ bool CampoMinado::TemBomba(int linha,int coluna)
     }
     return false;
 }
+bool CampoMinado::TemBombaPerto(int linha,int coluna)
+{
+    // Verificando apenas nas casas adjacentes
+    for (int i = linha - 1; i <= linha + 1; i++) {
+        for (int j = coluna - 1; j <= coluna + 1; j++) {
+            if (i >= 0 && i < 9 && j >= 0 && j < 9 && TemBomba(i, j)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int CampoMinado::ContaBomba(int linha,int coluna)
+{
+    int b = 0;
+    for (int i = linha - 1; i <= linha + 1; i++) {
+        for (int j = coluna - 1; j <= coluna + 1; j++) {
+            if (i >= 0 && i < 9 && j >= 0 && j < 9 && TemBomba(i, j)) {
+                b++;
+            }
+        }
+    }
+    return b;
+}
 void CampoMinado::AtualizarTabuleiro(int linha, int coluna)
 {
+    // Se a posição selecionada tiver bomba, colocá-la no tabuleiro e sair
+    if(TemBomba(linha,coluna)){                                            
+        tabuleiro[linha][coluna] = 'B';
+        return;
+    }
     
-    if(!TemBomba(linha,coluna)){
-        tabuleiro[linha][coluna] = ' ';
-       }
+    // Contar as bombas ao redor da posição selecionada
+    int bombas = ContaBomba(linha, coluna);
     
-   
+    // Se houver bombas ao redor, mostrar o número de bombas no tabuleiro e sair
+    if(bombas > 0){
+        tabuleiro[linha][coluna] = '0' + bombas;
+        return;
+    }
+    
+    // Se não houver bombas ao redor, mostrar a posição vazia no tabuleiro e atualizar o tabuleiro recursivamente
+    tabuleiro[linha][coluna] = ' ';
+    for (int i = linha - 1; i <= linha + 1; i++) {
+        for (int j = coluna - 1; j <= coluna + 1; j++) {
+            if (i >= 0 && i < 9 && j >= 0 && j < 9 && tabuleiro[i][j] == '-') {
+                AtualizarTabuleiro(i, j);
+            }
+        }
+    }
 }
 int main()
 {
     CampoMinado m;
     int row,column;
     m.Inicializar();
-    do{
+    string resposta;
+    bool jogando = true;
+    while(jogando){
         m.MostrarTabuleiro();
-        cout << "Linha: ";
-        cin >> row;
         cout << endl;
-        cout << "Coluna: ";
-        cin >> column;
-        cout << endl;
-        m.AtualizarTabuleiro(row,column);
-    }while(!m.TemBomba(row,column));
+        do{
+            cout << "Linha: ";
+            cin >> row;
+            row -= 1; 
+            cout << endl;
+        }while(row<0 || row>9 );
+        do{
+            cout << "Coluna: ";
+            cin >> column;
+            column -= 1;
+            cout << endl;
+        }while(column<0 || column>9 );
+        if(m.TemBomba(row,column)){
+            cout << "Voce perdeu!" << endl;
+            cout << "Deseja reiniciar?(sim/nao)";
+            cin >> resposta;
+            if(resposta == "sim"){
+                cout << endl;
+                m.Inicializar();
+            }else{
+                jogando = false;
+            }
+        }else{
+            m.AtualizarTabuleiro(row,column);
+        }
+    }
+    
     return 0;
 }
